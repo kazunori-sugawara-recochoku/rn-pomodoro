ddb28d671dd0d2e659af4735958b0746a64907c9
janus_wel <janus.wel.3@gmail.com>
2016-08-18 00:19:40 +0900

    chore(app): Unify App component

---
+++ src/components/app.js
@@ -0,0 +1,30 @@
+// @flow
+
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+
+export default function App() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>app</Text>
+      </View>
+    );
+}
